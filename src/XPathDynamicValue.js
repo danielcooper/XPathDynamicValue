33ce1c44e344cd8da6b84ec95fe985ec8c9f4893
daniel@14lines.com <daniel@14lines.com>
2020-10-21 06:22:06 +0100

    Add option to remove xmlns

--- src/XPathDynamicValue.js
+++ src/XPathDynamicValue.js
@@ -7,7 +7,8 @@ class XPath {
   static help = 'https://github.com/danielcooper/XPathDynamicValue'
   static inputs = [
     InputField("req", "Source Request", "Request"),
-    InputField("xpath", "XPath", "String")
+    InputField("xpath", "XPath", "String"),
+    InputField("removexmlns", "Remove namespace from output", "Checkbox")
   ]
 
   evaluate(context) {
@@ -16,7 +17,14 @@ class XPath {
     }
     const lastBody = this.req.getLastExchange().responseBody
     const documentNode = new DOMParser().parseFromString(lastBody, 'text/xml')
-    return xpath.select(this.xpath, documentNode)
+    const selected = xpath.select(this.xpath, documentNode)
+    if (selected){
+      var selectedString =  selected.toString()
+      if (this.removexmlns){
+        selectedString = selectedString.replace(/xmlns=".*?"/, '' );
+      }
+      return selectedString
+    }
   }
 
   title(_context){
