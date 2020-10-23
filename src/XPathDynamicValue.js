c77178e8cde33e550cbede7822c429fd6e7a369c
daniel@14lines.com <daniel@14lines.com>
2020-10-23 13:42:47 +0100

    When the xpath returns a list apply options on all elements and return

--- src/XPathDynamicValue.js
+++ src/XPathDynamicValue.js
@@ -14,31 +14,36 @@ class XPath {
     InputField("namespaces", "Namespaces", "KeyValueList", {"keyName":"namespace", "valueName":"url"})  ]
 
   evaluate(context) {
+    const options = this
+
     if (this.xpath === ""){
       return ""
     }
-    const lastBody = this.req.getLastExchange().responseBody
+    const lastBody = options.req.getLastExchange().responseBody
     const documentNode = new DOMParser().parseFromString(lastBody, 'text/xml')
 
     var selected = ""
-    var namespaces = this.namespaces.reduce(function(map, obj) {
+    var namespaces = options.namespaces.reduce(function(map, obj) {
         if (obj[2]){
           map[obj[0]] = obj[1];
         }
         return map;
       }, {});
     
-    selected = xpath.useNamespaces(namespaces)(this.xpath, documentNode);
+    selected = xpath.useNamespaces(namespaces)(options.xpath, documentNode);
 
     if (selected){
-      var selectedString =  selected.toString()
-      if (this.removexmlns){
-        selectedString = selectedString.replace(/xmlns=".*?"/, '' );
-      }
-      if (this.prettyprint){
-        selectedString = format(selectedString, {collapseContent: true})
-      }
-      return selectedString
+      mapped = selected.map(function(selectedItem) {
+        var selectedString =  selectedItem.toString()
+        if (options.removexmlns){
+          selectedString = selectedString.replace(/xmlns=".*?"/, '' );
+        }
+        if (options.prettyprint){
+          selectedString = format(selectedString, {collapseContent: true})
+        }
+        return selectedString
+       })
+       return mapped.join("\r")
     }
   }
 
